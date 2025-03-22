2f908497facb8838efd75eb4be76c6d76909b48c
arjav1528 <arjav1528@example.com>
2025-03-22 13:02:46 -0400

    Refactor app structure and add clinician and patient models

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,26 @@
 const express = require('express');
 const app = express();
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const healthCheckRouter = require('./Routes/HealthCheckRoutes/HealthCheckRoute');
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+
+
+
+//Routes
+
+app.use('/api/v1/health',healthCheckRouter);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
 
 
 
